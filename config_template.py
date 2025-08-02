a7ad3428bf35f49d055b82d13ecf115b9c75126c
kiesowd <damon@kiesow.net>
2025-08-01 21:36:01 -0500

    Clarified the desired export path in the config

--- config_template.py
+++ config_template.py
@@ -8,5 +8,5 @@ ITEM_ID = "YOUR_ITEM_ID"
 BOOK_BASE_URL = "https://bigbookstore.com"
 
 # The path (absolute or relative) where the XLS export will be saved (string)
-EXPORT_PATH = "booktrack_export.xlsx"
+EXPORT_PATH = "/path/to/booktrack_export.xlsx"
 
